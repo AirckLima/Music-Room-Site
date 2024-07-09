e05b7630da4f264c0c2773ef8a232c73ae7e0949
AirckLima <aircklima@gmail.com>
2024-07-09 12:57:29 -0300

    build: add Anime.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Monomaniac_One } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const monomaniac = Monomaniac_One({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,9 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={ inter.className }>
+      <body className={ monomaniac.className }>
         { children }
-      <footer className="w-full h-[10vh] bg-slate-400 rounded-t-xl">
+        <footer className="w-full h-[10vh] bg-slate-400 rounded-t-xl">
         </footer>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,30 @@
+import Divider from "@/components/Divider";
 import TopicSection from "@/components/TopicSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="grid grid-cols-1 min-h-screen flex-col items-center justify-between ">
-      <div className="flex flex-col justify-center items-center w-full h-[10vh] bg-slate-400"></div>
 
-      <TopicSection title="">
+      <Divider id="" icon={ false } />
+
+      <TopicSection>
       </TopicSection>
 
-        <div className="flex flex-col justify-center items-center w-full h-[10vh] bg-slate-400">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={ 1.5 } stroke="currentColor" className="size-2/5">
-          <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-        </svg>
-      </div>
+      <Divider id="" />
 
-      <section className="relative flex flex-col h-[80vh] w-full px-12 py-6 ">
-        <h1>1</h1>
-      </section>
+      <TopicSection title="about me">
+      </TopicSection>
 
-      <div className="flex flex-col justify-center items-center w-full h-[10vh] bg-slate-400">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={ 1.5 } stroke="currentColor" className="size-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-        </svg>
-      </div>
+      <Divider id="" />
 
-      <section className="relative flex flex-col h-[80vh] w-full px-12 py-6 ">
-        <h1>1</h1>
-      </section>
+      <TopicSection title="my portfolio">
+      </TopicSection>
 
-      <div className="flex flex-col justify-center items-center w-full h-[10vh] bg-slate-400">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={ 1.5 } stroke="currentColor" className="size-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-        </svg>
-      </div>
+      <Divider id="" />
 
-      <section className="relative flex flex-col h-[80vh] w-full px-12 py-6 ">
-        <h1>1</h1>
-      </section>
+      <TopicSection title="my hobbies">
+      </TopicSection>
 
     </main>
   );
